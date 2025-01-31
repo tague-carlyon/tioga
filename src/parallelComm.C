1ca26cd710568f2470284112717e042d62f1cf33
Robert Weston Tague Carlyon <tcarlyon@umd.edu>
2025-01-31 19:15:21 +0000

    Refactor sendRecvPackets function for improved readability and structure

--- src/parallelComm.C
+++ src/parallelComm.C
@@ -121,34 +121,33 @@ void parallelComm::sendRecvPackets(PACKET *sndPack,PACKET *rcvPack)
     MPI_Isend(&(scount[2*i]),2,MPI_INT,sndMap[i],tag,scomm,&request[irnum++]);
   //
   MPI_Waitall(irnum,request,status);
-  for(i=0;i<nrecv;i++)
-    {
-      rcvPack[i].nints=rcount[2*i];
-      rcvPack[i].nreals=rcount[2*i+1];
-    }
+  for(i=0;i<nrecv;i++) {
+    rcvPack[i].nints=rcount[2*i];
+    rcvPack[i].nreals=rcount[2*i+1];
+  }
   //
   irnum=0;
-  for(i=0;i<nrecv;i++)
-    {
-      if (rcvPack[i].nints > 0) {
-	tag=1;
-	rcvPack[i].intData=(int *) malloc(sizeof(int)*rcvPack[i].nints);
-	MPI_Irecv(rcvPack[i].intData,rcvPack[i].nints,
-		  MPI_INT,rcvMap[i],
-		  tag,scomm,&request[irnum++]);
-      }
-      if (rcvPack[i].nreals > 0) {
-	tag=2;
-	rcvPack[i].realData=(REAL *) malloc(sizeof(REAL)*rcvPack[i].nreals);
-	MPI_Irecv(rcvPack[i].realData,rcvPack[i].nreals,
-		  MPI_DOUBLE,rcvMap[i],
-		  tag,scomm,&request[irnum++]);
-      }
+  for(i=0;i<nrecv;i++) {
+    if (rcvPack[i].nints > 0) {
+      tag=1;
+      rcvPack[i].intData=(int *) malloc(sizeof(int)*rcvPack[i].nints);
+      MPI_Irecv(rcvPack[i].intData,rcvPack[i].nints,
+          MPI_INT,rcvMap[i],
+          tag,scomm,&request[irnum++]);
     }
+
+    
+    if (rcvPack[i].nreals > 0) {
+      tag=2;
+      rcvPack[i].realData=(REAL *) malloc(sizeof(REAL)*rcvPack[i].nreals);
+      MPI_Irecv(rcvPack[i].realData,rcvPack[i].nreals,
+          MPI_DOUBLE,rcvMap[i],
+          tag,scomm,&request[irnum++]);
+    }
+  }
   //
-  for(i=0;i<nsend;i++)
-    {
-      if (sndPack[i].nints > 0){
+  for(i=0;i<nsend;i++) {
+    if (sndPack[i].nints > 0){
 	tag=1;
 	MPI_Isend(sndPack[i].intData,sndPack[i].nints,
 		  MPI_INT,sndMap[i],
